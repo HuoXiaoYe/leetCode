2cb21e69218209eb0045c5d65c5be9f19516c64e
huoxiaoye <17711034@bjtu.edu.cn>
2019-12-07 21:30:20 +0800

    1

--- 01-10/01.两数之和.js
+++ 01-10/01.两数之和.js
@@ -24,4 +24,19 @@ const twoSum = (arr, target) => {
 }
 var nums = [2, 7, 11, 15];
 var target = 17
-console.log(twoSum(nums,target))
+console.log(twoSum(nums, target))
+
+/*
+    高手的解法
+var twoSum = function(nums, target) {
+    var temp=[];
+    for(var i=0; i<nums.length; i++){
+        var dif=target-nums[i];
+        if(temp[dif]!=undefined){
+            return [temp[dif], i];
+        }
+        temp[nums[i]]=i;
+    }
+}
+
+*/
