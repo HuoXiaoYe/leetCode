da0d95a2d0ed40917ff82331a1e202810c47e160
huoxiaoye <17711034@bjtu.edu.cn>
2019-12-11 20:51:00 +0800

    2

--- 01-10/02.两数相加.js
+++ 01-10/02.两数相加.js
@@ -79,3 +79,38 @@ var addTwoNumbers = function(l1, l2) {
 
 
 */
+
+/**
+ * @param {Integer} val
+ * @return {ListNode}
+ */
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+/**
+* @param {ListNode} l1
+* @param {ListNode} l2
+* @return {ListNode}
+*/
+var addTwoNumbers = function (l1, l2) {
+  let val = l1.val + l2.val;
+  let first = node = new ListNode(val % 10);
+
+  while ((l1 && l1.next) || (l2 && l2.next) || val >= 10) {
+      l1 = l1 && l1.next;
+      l2 = l2 && l2.next;
+
+      if (l1 || l2) {
+          val = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + (val >= 10 ? 1 : 0);
+          node = node.next = new ListNode(val % 10);
+          continue;
+      }
+
+      val = val % 10;
+      node = node.next = new ListNode(1);
+  }
+
+  return first;
+};
