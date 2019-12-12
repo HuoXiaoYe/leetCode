b379ae5baa3c9e35b24b90b595022fe019f5fd2a
huoxiaoye <17711034@bjtu.edu.cn>
2019-12-12 19:55:58 +0800

    15

--- 11-20/15.三数之和.js
+++ 11-20/15.三数之和.js
@@ -57,3 +57,31 @@ var threeSum = function(nums) {
     console.log(resArr)
     return Array.from(new Set(resArr.map(item => item.join('-')))).map(item => item.split('-'))
 };
+
+var threeSum = function (nums) {
+    let res = []
+    let length = nums.length;
+    nums.sort((a, b) => a - b) // 先排个队，最左边是最弱（小）的，最右边是最强(大)的
+    // 和为零 则一个数组中必定有的元素为正 有的元素为负
+    if (nums[0] <= 0 && nums[length - 1] >= 0) { // 优化1: 整个数组同符号，则无解
+        for (let i = 0; i < length - 2;) {
+            if (nums[i] > 0) break; // 优化2: 最左值为正数则一定无解
+            let first = i + 1
+            let last = length - 1
+            do {
+                if (first >= last || nums[i] * nums[last] > 0) break // 两人选相遇，或者三人同符号，则退出
+                let result = nums[i] + nums[first] + nums[last]
+                if (result === 0) { // 如果可以组队
+                    res.push([nums[i], nums[first], nums[last]])
+                }
+                if (result <= 0) { // 实力太弱，把菜鸟那边右移一位
+                    while (first < last && nums[first] === nums[++first]) { } // 如果相等就跳过
+                } else { // 实力太强，把大神那边左移一位
+                    while (first < last && nums[last] === nums[--last]) { }
+                }
+            } while (first < last)
+            while (nums[i] === nums[++i]) { }
+        }
+    }
+    return res
+}
---
+++ 11-20/16.最接近的三数之和.js
@@ -0,0 +1,70 @@
+/*
+给定一个包括 n 个整数的数组 nums 和 一个目标值 target。
+找出 nums 中的三个整数，使得它们的和与 target 最接近。
+返回这三个数的和。假定每组输入只存在唯一答案。
+
+例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+
+与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+// var threeSumClosest = function (nums, target) {
+//     nums.sort((a, b) => a - b);
+//     let sum = 0;
+//     let dif = Number.MAX_SAFE_INTEGER;
+//     // 定一个值
+//     for (var i = 0; i < nums.length - 1; i++) {
+//         let first = i + 1;
+//         let last = nums.length - 1;
+//         while (true) {
+//             // 退出条件？？
+//             if (first > last) {
+//                 break;
+//             }
+//             if (Math.abs(target - (nums[i] + nums[first] + nums[last])) < dif) {
+//                 sum = (nums[i] + nums[first] + nums[last])
+//                 dif = Math.abs(target - sum);
+//             } else {
+//                 first++;
+//                 last--;
+//             }
+//         }
+//     }
+//     return sum
+
+// };
+// [-1, 2, 1, -4]
+var threeSumClosest = function (nums, target) {
+    var sum = 0;
+    let dif = Number.MAX_SAFE_INTEGER;
+    var length = nums.length
+    for (var i = 0; i < length - 2; i++) {
+        let left = i + 1;
+        let right = length - 1;
+        while (left < right) {
+            // 
+            let tempSum = nums[i] + nums[left] + nums[right];
+            if (tempSum > target) {
+                right--
+            } else if (tempSum < target) {
+                left++
+            } else {
+                return tempSum;
+            }
+            console.log(tempSum,"===")
+            if (Math.abs(tempSum - target) < Math.abs(dif)) {
+                sum = tempSum;
+                dif = Math.abs(tempSum - target)
+            }
+        }
+    }
+    return sum
+};
+// -3 -4 1 ==> -3 -4  2 1 -4 => -1
+// 2
+console.log(threeSumClosest([-1, 2, 1, -4], 1))
