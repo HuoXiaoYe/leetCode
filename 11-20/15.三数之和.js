87911aa8fc343eda8f40f7a0736fcdf25dc1a7c0
huoxiaoye <17711034@bjtu.edu.cn>
2019-12-12 13:49:36 +0800

    15

--- 11-20/15.三数之和.js
+++ 11-20/15.三数之和.js
@@ -19,8 +19,8 @@
  */
 // 1. 有一个元素为 0 另两个互为相反数
 // 2. a + b = -c
-var threeSum = function (nums) {
-    nums.sort((a, b) => a - b);
+var threeSum = function(nums) {
+    nums.sort((a,b)=>a-b);
     var resArr = [];
     // 第一种情况
     // if(nums.indexOf(0)!==-1&&nums[0] < 0){ // 数组中有元素 0 并且数组中有负数
@@ -43,16 +43,17 @@ var threeSum = function (nums) {
     // 我现在已经把数组从小到大排序了
     // 
     let index = 0;
-    while (nums[index] <= 0) {
-        for (var j = index + 1; j < nums.length; j++) {
+    while(nums[index]<=0){
+        for(var j=index+1;j<nums.length;j++){
             let dif = 0 - nums[index] - nums[j]
-            if (nums.lastIndexOf(dif) !== -1 && nums.indexOf(dif) !== j && j < nums.lastIndexOf(dif)) { // 存在其相反数
+            if(nums.lastIndexOf(dif)!==-1&&nums.lastIndexOf(dif)!==j&&j<nums.lastIndexOf(dif)){ // 存在其相反数
                 resArr[resArr.length] = [];
-                resArr[resArr.length - 1].push(nums[index], nums[j], dif)
+                resArr[resArr.length-1].push(nums[index],nums[j],dif)
                 break
             }
         }
-        index++
+        index ++
     }
-    return resArr
+    console.log(resArr)
+    return Array.from(new Set(resArr.map(item => item.join('-')))).map(item => item.split('-'))
 };
