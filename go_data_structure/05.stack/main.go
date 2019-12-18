5e664b6667005cbe70cffec432e67482eff89d07
huoxiaoye <17711034@bjtu.edu.cn>
2019-12-18 10:17:23 +0800

    stack

---
+++ go_data_structure/05.stack/main.go
@@ -0,0 +1,73 @@
+package main
+
+import "fmt"
+
+//入栈和出栈操作
+type Stack struct {
+	maxSize int
+	data    []int
+	top     int
+}
+
+// 栈初始化
+func (s *Stack) initStack(maxSize int) {
+	s.data = make([]int, maxSize)
+}
+
+//判断栈是否为空
+func (s *Stack) isEmpty() bool {
+	return s.top == 0
+}
+
+// 判断栈是否已满
+func (s *Stack) isFull() bool {
+	return s.top == s.maxSize
+}
+
+//返回栈内元素的个数
+func (s *Stack) length() int {
+	return s.top
+}
+
+//入栈
+func (s *Stack) push(val int) {
+	if s.isFull() {
+		fmt.Println("栈已满")
+		return
+	}
+	s.data[s.top] = val
+	s.top++
+}
+
+// 出栈
+func (s *Stack) pop() int {
+	// 判断栈是否为空
+	if s.isEmpty() {
+		fmt.Println("栈为空")
+		return 999999999
+	}
+	s.top--
+	var rs = s.data[s.top]
+	return rs
+}
+
+func main() {
+	var s *Stack = &Stack{
+		maxSize: 5,
+	}
+	s.initStack(5)
+	// fmt.Println(s.isEmpty())
+	s.push(1)
+	s.push(2)
+	s.push(3)
+	s.push(4)
+	s.push(5)
+	fmt.Println(s.pop())
+	fmt.Println(s.pop())
+	fmt.Println(s.pop())
+	fmt.Println(s.pop())
+	fmt.Println(s.pop())
+	fmt.Println(s.pop())
+	// fmt.Println(s.top)
+	// fmt.Println(s.maxSize)
+}
