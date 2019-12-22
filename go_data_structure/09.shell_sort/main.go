dcee774890763326563b10793b0a1141fe790f69
huoxiaoye <17711034@bjtu.edu.cn>
2019-12-22 10:30:35 +0800

    bucket sort

--- go_data_structure/09.shell_sort/main.go
+++ go_data_structure/09.shell_sort/main.go
@@ -6,8 +6,8 @@ import "fmt"
 
 func shellSort(arr []int) []int {
 	for gap := len(arr) / 2; gap > 0; gap /= 2 {
-		for i := 0; i < gap; i++ { // 一共分为了gap个组，既循环gap次
-			// 跳出条件是什么
+		for i := 0; i < gap; i++ { // 一共分为了gap个组，既循环gap次 循环每一个组
+			// 内部跳出条件是什么
 			for j := i; j < len(arr)-gap; j += gap { // 第i个组
 				if arr[j] > arr[j+gap] {
 					var temp = arr[j]
---
+++ go_data_structure/10.bucket_sort/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+type MyIntSort []int
+
+func (m MyIntSort) Len() int {
+	return len(m)
+}
+
+func (m MyIntSort) Less(i, j int) bool {
+	return m[i] < m[j]
+}
+
+func (m MyIntSort) Swap(i, j int) {
+	m[i], m[j] = m[j], m[i]
+}
+
+func bucketSort(arr []int) []int {
+
+	// 找到最大值的length
+	var max = arr[0]
+	for i := 1; i < len(arr); i++ {
+		if max < arr[i] {
+			max = arr[i]
+		}
+	}
+	// fmt.Println(max)
+	var maxElLen = len(string(max)) // 最大数字的长度
+
+	for i := 0; i < maxElLen; i++ { // i 代表 个位数 十位数 百位数的循环
+		var tempIndex = 0
+		var bucket [10]MyIntSort
+		var temp = math.Pow(10, float64(i))
+		for j := 0; j < len(arr); j++ { // 将数据装入到每一个桶子中
+			var cur = arr[j] / int(temp) % 10
+			bucket[cur] = append(bucket[cur], arr[j])
+		}
+		for k := 0; k < len(bucket); k++ {
+			if len(bucket[k]) != 0 { //说明这个筒子中有数据
+				sort.Sort(bucket[k])
+				// 将排序好的桶子中的数据放入到 arr中
+				for m := 0; m < len(bucket[k]); m++ {
+					arr[tempIndex] = bucket[k][m]
+					tempIndex++
+				}
+			}
+		}
+	}
+
+	return arr
+}
+
+func main() {
+	var arr []int = []int{1, 45, 2, 15, 792, 1642, 7798, 456, 123}
+	var rs = bucketSort(arr)
+	fmt.Println(rs)
+}
