288dabad15da2f8b750cd505367c51164c135b39
zhangtuanjie <zhangtuanjie@meituan.com>
2020-03-08 22:31:13 +0800

    24

--- 21-30/24.两两交换链表中的节点.js
+++ 21-30/24.两两交换链表中的节点.js
@@ -1,11 +1,3 @@
-/*
-给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
-你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
-
-示例:
-给定 1->2->3->4, 你应该返回 2->1->4->3.
-*/
-
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -18,16 +10,15 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-    if(head == null) return head
-    let temp1 = head;
-    let temp2 = head.next
-    // 长度为奇数  需要考虑吗？？
-    while(true){
-        if(temp1==null||temp2==null) break
-        // js中没有指针 怎么进行节点交换呢
-        [temp1.val, temp2.val] = [temp2.val,temp1.val]
-        temp1 = temp2.next;
-        if(temp1) temp2 = temp1.next
+    if(head ==null || head.next == null) return head;
+    let dummyHead = p = new ListNode();
+    p.next = head;
+    let node1,node2;
+    while((node1 = p.next) && (node2 = p.next.next)){
+        node1.next = node2.next;
+        node2.next = node1;
+        p.next = node2;
+        p = node1;
     }
-    return head
+    return dummyHead.next
 };
